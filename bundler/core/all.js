1b41fc6f3f2f062a6a651fdda10d25409c88acf5
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2022-02-03 13:14:56 +0800

    Specify external packages

--- bundler/core/all.js
+++ bundler/core/all.js
@@ -4,10 +4,16 @@ import vue from "@vitejs/plugin-vue";
 export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
 	return [
 		{
+			"external": [
+				"vue"
+			],
 			"input": "src/index.js",
 			"output": {
 				"file": "dist/index.js",
 				"format": "iife",
+				"globals": {
+					"vue": "Vue"
+				},
 				"interop": "auto",
 				"name": "app"
 			},
