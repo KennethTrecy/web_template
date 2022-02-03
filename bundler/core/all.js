109fbaf412ed236598218fececbe03cd2645ca42
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2022-02-03 13:38:13 +0800

    Bundle third-party packages

--- bundler/core/all.js
+++ bundler/core/all.js
@@ -1,5 +1,8 @@
+/* eslint-disable max-lines-per-function */
 import { DEVELOPMENT } from "./environments";
 import alias from "@rollup/plugin-alias";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
@@ -27,6 +30,29 @@ export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
 				}),
 				vue({
 
+				}),
+				...generalPostPlugins
+			]
+		},
+		{
+			"input": "third-party/vue.js",
+			"output": {
+				"file": "dist/third-party/vue.js",
+				"format": "iife",
+				"interop": "auto",
+				"name": "Vue"
+			},
+			"plugins": [
+				nodeResolve({
+					"browser": true
+				}),
+				replace({
+					"preventAssignment": false,
+					"values": {
+						"__VUE_OPTIONS_API__": "false",
+						"__VUE_PROD_DEVTOOLS__": `${environment === DEVELOPMENT}`,
+						"process.env.NODE_ENV": `"${environment}"`
+					}
 				}),
 				...generalPostPlugins
 			]
