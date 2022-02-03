669915b50ed13d8cd1576da4ba2b6bfd0d12bf73
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2022-02-03 12:56:46 +0800

    Create sample code

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+	<span>{{ message }}</span>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import { ref } from "vue";
+
+export default {
+	setup() {
+		const message = ref("Hello World!");
+
+		return {
+			message
+		};
+	}
+};
+</script>
