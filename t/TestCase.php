63533a0d06637945afe51e1f0ccfc13ea8925ae8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-05 15:50:29 +0800

    Add template for base test case

---
+++ t/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase {
+	protected function defineEnvironment($app) {
+
+	}
+
+	protected function getPackageProviders($app) {
+		return [
+
+		];
+	}
+}
