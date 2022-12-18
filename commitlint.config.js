91b6a0dee54e1ed0c6cc657c212e81ee8849965f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-18 20:31:16 +0800

    dx: increase the maximum line length of body

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,9 @@ module.exports = {
 	"extends": [ "@commitlint/config-conventional" ],
 	"rules": {
 		"body-case": [ 2, "always", "sentence-case" ],
-		"body-max-line-length": [ 2, "always", 72 ],
+		// It is better to limit to 72 characters. However, the limit was increased to allow insertion
+		// of long links.
+		"body-max-line-length": [ 2, "always", 150 ],
 		"signed-off-by": [ 2, "always", "" ],
 		"type-enum": [
 			2,
