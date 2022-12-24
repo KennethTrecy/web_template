1b4b4bcea4369ed20146ef57806e070d09246966
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-25 02:00:43 +0800

    build(typescript): add plugin to compile Typescript

--- bundler/core/all.js
+++ bundler/core/all.js
@@ -1,16 +1,32 @@
+import { join } from "path"
+import esbuild from "rollup-plugin-esbuild-transform"
 import { DEVELOPMENT } from "./environments";
+
+const ROOT = join(__dirname, "../..")
+const TYPESCRIPT_CONFIGURATION = "tsconfig.json"
 
 export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
 	return [
 		{
-			"input": "src/index.js",
+			"input": "src/index.ts",
 			"output": {
 				"file": "dist/index.js",
 				"format": "iife",
 				"interop": "auto",
 				"name": "app"
 			},
-			"plugins": [
+			"plugins": [,
+				esbuild([
+					{
+						"loader": "ts",
+						"tsconfig": join(ROOT, TYPESCRIPT_CONFIGURATION)
+					},
+					{
+						"loader": "js",
+						// "minify": true,
+						"output": true
+					}
+				]),
 				...generalPostPlugins
 			]
 		}
