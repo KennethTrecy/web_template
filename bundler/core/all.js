9ccd982de9d36d3fed0d3ee8ba2dcdb65c4bea34
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-25 11:56:20 +0800

    build(mocha-svelte): generalize the pipeline for testing

--- bundler/core/all.js
+++ bundler/core/all.js
@@ -3,10 +3,44 @@ import autoPrepocess from "svelte-preprocess"
 import commonjs from "@rollup/plugin-commonjs"
 import nodeResolve from "@rollup/plugin-node-resolve"
 
-import { DEVELOPMENT } from "./environments";
+import { PRODUCTION, DEVELOPMENT, TEST } from "./environments";
 
 export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
-	return [
+	const commonPipeline = [
+		svelte({
+			"compilerOptions": {
+				"dev": environment === DEVELOPMENT || environment === TEST
+			},
+			"preprocess": autoPrepocess({
+
+			})
+		}),
+		nodeResolve({
+			"browser": environment === PRODUCTION || environment === DEVELOPMENT,
+			"exportConditions": [ "svelte" ],
+			"extensions": [ ".svelte" ],
+			"dedupe": [ "svelte" ]
+		}),
+		commonjs(),
+	]
+
+	return environment === TEST
+	? [
+		{
+			"input": "src/**/*.spec.ts",
+			"output": {
+				"file": "hidden/index.js",
+				"format": "cjs",
+				"interop": "auto",
+				"name": "tests"
+			},
+			"plugins": [
+				...commonPipeline,
+				...generalPostPlugins
+			]
+		}
+	]
+	: [
 		{
 			"input": "src/index.js",
 			"output": {
@@ -16,18 +50,7 @@ export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
 				"name": "app"
 			},
 			"plugins": [
-				svelte({
-					"preprocess": autoPrepocess({
-
-					})
-				}),
-				nodeResolve({
-					"browser": true,
-					"exportConditions": [ "svelte" ],
-					"extensions": [ ".svelte" ],
-					"dedupe": [ "svelte" ]
-				}),
-				commonjs(),
+				...commonPipeline,
 				...generalPostPlugins
 			]
 		}
