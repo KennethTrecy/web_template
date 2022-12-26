61fc01192b012088f9240bd7568c38e887fab1c2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-26 11:14:30 +0800

    chore(master): remove files from other stacks

--- web/src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai"
-
-describe("Operation", function() {
-	describe("Addition", function() {
-		it("should return 2", function() {
-			expect(1+1).to.equal(2);
-		});
-	});
-});
