4377c889dec30842f2d6901e3877a3c3470dd7c9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-09 21:46:59 +0800

    test(sveltekit): specialize test code

--- t/unit/index.ts
+++ t/unit/index.ts
@@ -1,9 +1,9 @@
 // @vitest-environment jsdom
 
-import { cleanup, render } from "@testing-library/vue";
+import { cleanup, render } from "@testing-library/svelte";
 import { expect, it } from "vitest";
 
-import App from "@/app.vue";
+import App from "@/routes/+page.svelte";
 
 it("can test", () => {
 	const { container } = render(App, {
