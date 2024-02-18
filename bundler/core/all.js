1ff48caa0ef31e7b5eb81903e302fadda79956c9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-02-18 14:28:34 +0800

    chore(typescript): specify the configuration to bundle files

--- bundler/core/all.js
+++ bundler/core/all.js
@@ -1,8 +1,10 @@
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
 import { DEVELOPMENT, PRODUCTION } from "./environments.js"
 import esbuild from "rollup-plugin-esbuild"
 
-const ROOT = join(__dirname, "../..")
-const TYPESCRIPT_CONFIGURATION = "tsconfig.json"
+export const ROOT = join(dirname(fileURLToPath(import.meta.url)), "../..")
+export const TYPESCRIPT_CONFIGURATION = join(ROOT, "tsconfig.json")
 
 export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
 	return [
@@ -10,13 +12,14 @@ export default function(environment = DEVELOPMENT, generalPostPlugins = []) {
 			"input": "src/index.ts",
 			"output": {
 				"file": "dist/index.js",
-				"format": "iife",
+				"format": "esm",
 				"interop": "auto",
 				"name": "app"
 			},
 			"plugins": [
 				esbuild({
-					"minify": environment === PRODUCTION
+					"minify": environment === PRODUCTION,
+					"tsconfig": TYPESCRIPT_CONFIGURATION
 				}),
 				...generalPostPlugins
 			]
