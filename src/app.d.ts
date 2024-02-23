6d957a00f24e4db9aca88e7401347a3ab2d8a85a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-02-23 22:51:34 +0800

    internal(sveltekit): add page state interface
    It seems that this is new interface after the update.
    
    Reference: https://github.com/sveltejs/kit/blob/4ae1cd152a60a22044ab3b8aa5572a2b79864583/packages/create-svelte/templates/skeleton/src/app.d.ts#L8

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,6 +5,7 @@ declare namespace App {
 	// interface Error {}
 	// interface Locals {}
 	// interface PageData {}
+	// interface PageState {}
 	interface Platform {
 		env: {
 			// Public environment variables here...
