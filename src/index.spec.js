ef6012c19442492476c9a7f54c190d887446d482
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-02-24 00:10:08 +0800

    test(mocha): change the import method to test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,9 @@
-var { expect } = require("chai");
+const chai = import("chai");
 
 describe("Operation", function() {
 	describe("Addition", function() {
-		it("should return 2", function() {
+		it("should return 2", async function() {
+			const { expect } = await chai
 			expect(1+1).to.equal(2);
 		});
 	});
